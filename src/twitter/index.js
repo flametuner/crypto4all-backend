af895aba5c60222b79eb8494433fb37c30b971bf
Gustavo Inacio <incio.gusta@gmail.com>
2021-11-26 12:43:11 -0300

    refact: use function for multi-ids

--- src/twitter/index.js
+++ src/twitter/index.js
@@ -6,13 +6,21 @@ var client = new Twitter({
   bearer_token: process.env.TWITTER_BEARER_TOKEN,
 });
 
-async function getTweetData(tweet_id) {
+const regex = /0x[a-fA-F0-9]{40}/;
+
+/**
+ * Get Tweet Data from a list of tweet IDs
+ * @param {[string]} tweet_ids
+ * @returns list of objects containing tweet author username, tweet text, and address present in tweet
+ * @on_error return list of ids with error
+ */
+async function getTweetData(tweet_ids) {
   const {
     data: tweet,
     errors,
     includes,
   } = await client.get("tweets", {
-    ids: tweet_id,
+    ids: tweet_ids,
     expansions: ["author_id"],
     tweet: {
       fields: ["created_at", "author_id", "text"],
@@ -22,18 +30,33 @@ async function getTweetData(tweet_id) {
     },
   });
 
+  const success_dict = {};
+  const error_list = [];
   if (errors) {
     console.log("Errors:", errors);
-    return;
+    for (let error of errors) {
+      error_list.push(error.value);
+    }
   }
-  console.log("Tweet:", tweet);
-  const regex = /0x[a-fA-F0-9]{40}/;
-  const address = regex.exec(tweet[0].text)[0];
-  return {
-    userNameTwitter: includes.users[0].username,
-    address,
-    content: tweet[0].text,
-  };
+  if (tweet) {
+    console.log("Tweets:", tweet);
+    const zipped = tweet.map(function (e, i) {
+      return [e, includes.users[i]];
+    });
+    for (const [t, u] of zipped) {
+      const address = regex.exec(t.text)[0];
+      if (address) {
+        success_dict[t.id] = {
+          userNameTwitter: u.username,
+          address,
+          content: t.text,
+        };
+      } else {
+        error_list.push(t.id);
+      }
+    }
+  }
+  return { success_dict, error_list };
 }
 
 module.exports = {
